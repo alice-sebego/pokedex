5c53d7ea499176984105b443febb00f93324968b
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-03-28 00:06:59 +0100

    Create a module which handle backgroundColor for each card

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
+import {setBackgroundColor} from './bgcolor-card.js';
+
 // DOM's elements
 const $main = document.querySelector("main");
 const $inputForm = document.querySelector("#pokemon");
-let $searchValue = document.querySelector("#pokemon").value;
-let inputValue = $searchValue.toLowerCase();
 const $form = document.querySelector("form");
 
 // URL & endpoints
@@ -77,55 +77,8 @@ const fetchInfoEachPokemon = async (pokemon) => {
             const $card = document.createElement("div");
             $card.classList.add("card");
             $card.innerHTML = `<img src="${picture}" alt="${name}"/><h2>${name}</h2><p>Id #${idPokemon}</p>`;
-            switch (typePokemon){
-                case "grass":
-                    $card.style.backgroundColor = "#78c850";
-                    break;
-                case "fire":
-                    $card.style.backgroundColor = "#F58271";
-                    break;
-                case "water":
-                    $card.style.backgroundColor = "#6390F0";
-                    break;
-                case "bug":
-                    $card.style.backgroundColor = "#B3F594";
-                    break;
-                case "normal":
-                    $card.style.backgroundColor = "#D9D5D8";
-                    break;
-                case "poison":
-                    $card.style.backgroundColor = "#966DA3";
-                    break;
-                case "electric":
-                    $card.style.backgroundColor = "#F7D02C";
-                    break;
-                case "ground":
-                    $card.style.backgroundColor = "#E2BF65";
-                    break;
-                case "fairy":
-                    $card.style.backgroundColor = "#D685AD";
-                    break;
-                case "figthing":
-                    $card.style.backgroundColor = "#C25956";
-                    break;
-                case "psychic":
-                    $card.style.backgroundColor = "#F95587";
-                    break;
-                case "rock":
-                    $card.style.backgroundColor = "#B6A136";
-                    break;
-                case "ghost":
-                    $card.style.backgroundColor = "#735797";
-                    break;
-                case "dragon":
-                    $card.style.backgroundColor = "#6F35FC";
-                    break;
-                case "ice":
-                    $card.style.backgroundColor = "#96D9D6";
-                default:
-                    $card.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
-                    break;
-            }
+            
+            setBackgroundColor(typePokemon, $card)
 
             $main.appendChild($card);
 
---
+++ src/js/bgcolor-card.js
@@ -0,0 +1,53 @@
+const setBackgroundColor = (type, card) => {
+    switch (type){
+        case "grass":
+            card.style.backgroundColor = "#78c850";
+            break;
+        case "fire":
+            card.style.backgroundColor = "#F58271";
+            break;
+        case "water":
+            card.style.backgroundColor = "#6390F0";
+            break;
+        case "bug":
+            card.style.backgroundColor = "#B3F594";
+            break;
+        case "normal":
+            card.style.backgroundColor = "#D9D5D8";
+            break;
+        case "poison":
+            card.style.backgroundColor = "#966DA3";
+            break;
+        case "electric":
+            card.style.backgroundColor = "#F7D02C";
+            break;
+        case "ground":
+            card.style.backgroundColor = "#E2BF65";
+            break;
+        case "fairy":
+            card.style.backgroundColor = "#D685AD";
+            break;
+        case "figthing":
+            card.style.backgroundColor = "#C25956";
+            break;
+        case "psychic":
+            card.style.backgroundColor = "#F95587";
+            break;
+        case "rock":
+            card.style.backgroundColor = "#B6A136";
+            break;
+        case "ghost":
+            card.style.backgroundColor = "#735797";
+            break;
+        case "dragon":
+            card.style.backgroundColor = "#6F35FC";
+            break;
+        case "ice":
+            card.style.backgroundColor = "#96D9D6";
+        default:
+            card.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+            break;
+    }
+}
+
+export {setBackgroundColor};
